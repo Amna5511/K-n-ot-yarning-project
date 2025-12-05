df9a7155a752bd1221988f5c2181081c1fba5c27
Emna <amnina2001@icloud.com>
2025-12-05 10:19:59 +0100

    js counter fix + html platform

--- javascript/platform.js
+++ javascript/platform.js
@@ -55,18 +55,31 @@ COUNT_DIS.textContent= 0;
 
 BTN_PLUS.addEventListener('click', () => { 
 
-      count = count++;
+     count++;
       COUNT_DIS.textContent=count;
-      console.log(count++)
+     // console.log(count++)
 })
 BTN_MINUS.addEventListener('click', () => { 
      // COUNT_DIS.textContent=count;
-      count = count - 1;
-      if((count < 0)){
-         
-      COUNT_DIS.textContent=0;
+      
 
-      }else((count - 1))
+      if(count < 0){
+      //
+      count --;
+
+        COUNT_DIS.textContent=count;
+      } else{
+
+        count = 0;
+      }
+      console.log(count)
+
+
+     // if(count < 0){
+      //   count = count - 1;
+      
+
+     // }COUNT_DIS.textContent=0;
       //console.log(count)
 })
 BTN_RESET.addEventListener('click', () => { 
